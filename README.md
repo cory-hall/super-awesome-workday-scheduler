25571d30f03a3b36da361fbbe4c5bcdd4a87fd8d
Cory Hall <ComebackCory1@gmail.com>
2022-03-11 16:46:07 -0600

    finished js functionality

--- README.md
+++ README.md
@@ -1,46 +1,17 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Super Awesome Workday Scheduler 
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+## Description
+This is a web app designed for productivity and organization. It is setup for a stand workday (9-5)
+and has features like persistent data via localStorage, color coded time blocks based on what time it is
+and easy to understand user interface.
 
-The starter code uses the [Moment.js](https://momentjs.com/) library to work with date and time, but you're free to use a different JavaScript solution to handle this functionality because Moment.js is considered a "legacy" product. Learn more about these other solutions in the [Moment.js project status page.](https://momentjs.com/docs/#/-project-status/).
+## Installation
+No special instructions required, simply go to https://cory-hall.github.io/super-awesome-workday-scheduler/
+and you'll have everything you need.
 
-## User Story
+## Screenshot
+<img src="assets\images\week-5-challenge.png" width=75% height=75%> <br>
 
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
-
-The following animation demonstrates the application functionality:
-
-![Work Day Scheduler app with color-coded time slots shows a new event being typed in the 5PM slot.](./assets/images/05-third-party-apis-homework-demo.gif)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
----
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+## Credit
+All JavaScript done by Cory Hall
+Most HTML and CSS supplied by boot camp staff
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,58 +1,81 @@
+// GLOBAL VARIABLES //
+
+// variable to access the container element
 var containerBlockEl = document.querySelector(".container");
+// variable to access the save button element
 var saveBtnEl = $(".saveBtn");
+// variable to access the current day element
 var currentDayEl = document.querySelector("#currentDay");
-var hourNow = moment().format('H').toString();
+// variable to get current hour
+var hourNow = moment().format('H');
+// array for holding and manipulating localStorage
 var allData = [];
 
-// this function loops through all possible hours, assigns whether its AM or PM and calls the buildRow function
+// loops through loop and calls buildRow function on each loop
 function buildAllRows() {
    // display todays date
    getDate();
 
    // loop through 8 hours to create rows
-   for (i = 9; i <= 17; i++) {
-      if (i < 12) {
-         buildRow(i, "AM");
-      } else {
-         buildRow(i, "PM");
-      }
+   // loop starts at 9 for 9AM and continues to 18, formatted for 24-hour time
+   // this i variable is used as hour in various functions for ids for each html element
+   for (var i = 9; i < 18; i++) {
+      buildRow(i, "");
    }
 };
 
 // this function creates each individual row of index.html
-function buildRow(hour, timeOfDay) {
+function buildRow(hour) {
+   // create each different html tag
    var hourBlockEl = document.createElement("p");
    var descBlockEl = document.createElement("textarea");
    var saveBtnEl = document.createElement("button");
    var timeBlockEl = document.createElement("div");
 
-   // create rows for the page
+   // create rows (time block) for the page
    timeBlockEl.className = "row time-block";
    timeBlockEl.setAttribute("id", hour);
 
    // create hour portion of time block
    // adjust 24-hour time to standard 12-hour time
+
+   // if hour is greater than 12, its afternoon, therefor PM is required text
+   // also subtracts 12 from hour to display in 12-hour time
    if (hour > 12) {
+      var timeOfDay = "PM"
       var tempHour = hour - 12;
       hourBlockEl.className = "hour col-2";
       hourBlockEl.setAttribute("id", hour);
       hourBlockEl.textContent = tempHour + " " + timeOfDay;
-      // append to hour block
+      // append to time block
       timeBlockEl.appendChild(hourBlockEl);
-   } else {
+
+   // if hour is less than 12, its morning, therefor AM is required text
+   } else if (hour < 12) {
+      var timeOfDay = "AM"
       hourBlockEl.className = "hour col-2";
       hourBlockEl.setAttribute("id", hour);
       hourBlockEl.textContent = hour + " " + timeOfDay;
-      // append to hour block
+      // append to time block
+      timeBlockEl.appendChild(hourBlockEl);
+   }
+   // else its noon, therefor PM is required text
+   else {
+      var timeOfDay = "PM"
+      hourBlockEl.className = "hour col-2";
+      hourBlockEl.setAttribute("id", hour);
+      hourBlockEl.textContent = hour + " " + timeOfDay;
+      // append to time block
       timeBlockEl.appendChild(hourBlockEl);
    }
 
    // create text portion of time block
    descBlockEl.setAttribute("id", hour);
+   // calls setBlockColors to determine the color of the descBlockEl
+   // based on what hour the block refers to and what time it currently is
    setBlockColors(descBlockEl);
    descBlockEl.classList.add("description");
    descBlockEl.classList.add("col-8");
-
 
    // append to time block
    timeBlockEl.appendChild(descBlockEl);
@@ -60,6 +83,7 @@ function buildRow(hour, timeOfDay) {
    // create save button of time block
    saveBtnEl.className = "saveBtn col-2";
    saveBtnEl.setAttribute("id", hour);
+   saveBtnEl.innerHTML = '<i class="far fa-save"></i>';
    // append to time block
    timeBlockEl.appendChild(saveBtnEl);
 
@@ -73,51 +97,102 @@ function getDate() {
    // current day
    var now = moment().format('dddd, MMMM D');
 
-   // append current day to index.html
+   // append current day to currentDayEl
    currentDayEl.append(now);
 };
 
+// this function determines what color each description block will be based on
+// what time it is
 function setBlockColors(descBlockEl) {
-   var hour = descBlockEl.getAttribute("id")
-   console.log(hour);
-   console.log(hourNow)
+   // get id, or what hour, in 24-hour format
+   var hour = descBlockEl.getAttribute("id");
+   hour = parseInt(hour);
 
+   // if id from block is equal to the time
+   // add present class to the descBlockEl
    if (hour == hourNow) {
       return descBlockEl.classList.add("present");
    }
+   // if id from block is greater than the time
+   // add future class to the descBlockEl
    else if (hour > hourNow) {
       return descBlockEl.classList.add("future");
    }
+   // else id from block is less than the time
+   // add past class to the descBlockEl
    else {
       return descBlockEl.classList.add("past");
    }
 }
 
-// this function is used to save individual time blocks to localStorage
+// this function is called on each save button click to save the respective
+// time block to localStorage
 function saveHour(id) {
+   // getting the textarea that matches the passed id parameter from the button click
    var desc = document.querySelector("textarea[id='" + id + "'").value;
 
+   // temp created object to be stored in allData array
    var timeBlock = {
       "hour": id,
       "desc": desc
    };
+
+   // if allData contains data
+   if (allData) {
+      // loop over the array
+      for (var i = 0; i < allData.length; i++) {
+         // if allData key hour at index i matches the requested timeBlock hour addition
+         if (allData[i].hour == timeBlock.hour) {
+            // remove the old desc from allData and replace with new desc
+            // this keeps the array to a max length of 8
+            allData.splice(i, 1);
+         }
+      }
+   }
+
+   // push timeBlock object to allData array
    allData.push(timeBlock);
+
+   // set allData array to localStorage
    localStorage.setItem("timeBlock", JSON.stringify(allData));
 };
 
 // this function is used to load saved time blocks from localStorage
 function loadHour() {
-   var tempArray = []
+   // variable used to select ALL the textarea elements
+   var descBlockEl = document.querySelectorAll(".description");
+   // update allData array with localStorage data
+   allData = JSON.parse(localStorage.getItem("timeBlock"));
 
-   tempArray = JSON.parse(localStorage.getItem("timeBlock"));
-
-   if (tempArray) {
-      allData.push(tempArray);
+   // if allData contains data
+   if (allData) {
+      // double loop to iterate through localStorage and compare to textarea ids
+      for (var i = 0; i < allData.length; i++) {
+         // the var j loop is used to check each descBlockEl against every individual var i loop
+         for (var j = 0; j < descBlockEl.length; j++) {
+            // if the hour key in allData at index i is equal to the id of textarea element
+            // at index j
+            if (allData[i].hour == descBlockEl[j].getAttribute("id")) {
+               // variable used to store the desc data saved in allData at index i
+               var inText = allData[i].desc;
+               // variable used to store which textarea element is being targeted
+               var inTarget = descBlockEl[j];
+               // add the text from allData array to the targeted textarea
+               inTarget.textContent = inText;
+            }
+         }
+      }
+   
+   } 
+   // else, set all data to empty
+   else {
+      allData = [];
    }
 }
 
 // event listener to populate the page on load
 addEventListener("load", buildAllRows);
+// event listener to load the localStorage on load
 addEventListener("load", loadHour);
 
 // event listener to save the time block on button click
